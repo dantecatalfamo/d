e96b353e48d0ef477325154cb2a5a99a56933737
Dante Catalfamo <dante.catalfamo@gmail.com>
2020-07-12 00:01:29 -0400

    rename to repo

--- repo.sh
+++ repo.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 
-dhome=~/src/github.com/dantecatalfamo/d
+repohome=~/src/github.com/dantecatalfamo/repo
 
-function d {
+function repo {
     case $1 in
         cl*)
-            d-clone $@
+            repo-clone $@
             ;;
         cd)
-            d-cd $@
+            repo-cd $@
             ;;
         *)
-            d-usage
+            repo-usage
             ;;
     esac
 }
 
-function d-usage {
-    echo "usage: d <command> [<args>]"
+function repo-usage {
+    echo "usage: repo <command> [<args>]"
     echo
     echo "Commands:"
     echo "  cd      Change to a project directory under ~/src"
     echo "  clone   Clone a repository into ~/src according to the site, user, and project"
 }
 
-function d-clone {
+function repo-clone {
     repo=$2
     if [ x$repo == x ];then
         echo "Repo required"
@@ -32,7 +32,7 @@ function d-clone {
     fi
 
     echo "Cloning" $repo
-    output=$(raku $dhome/clone.raku $repo)
+    output=$(raku $repohome/clone.raku $repo)
 
     if [ $? -eq 0 ]; then
         cd $output
@@ -43,10 +43,10 @@ function d-clone {
     fi
 }
 
-function d-cd {
+function repo-cd {
     search=$2
 
-    output=$(raku $dhome/cd.raku $search)
+    output=$(raku $repohome/cd.raku $search)
 
     if [ $? -eq 0 ]; then
         cd $output
